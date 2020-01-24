59f46854d822b36ad62320918e3775d9c5893a22
Ilya Valchanka <IValchanka@cpaglobal.com>
2020-01-24 17:17:35 +0300

    Created a simple solution for investigating Google Cloud Pub/Sub.

---
+++ PublisherApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Cloud.PubSub.V1;
+using Google.Protobuf;
+
+namespace PublisherApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().RunPublisherAsync().Wait();
+
+            Console.WriteLine("\n\nTap to continue...");
+            Console.ReadKey();
+        }
+
+        private async Task RunPublisherAsync()
+        {
+            // The GOOGLE_APPLICATION_CREDENTIALS environment variable contains the path to the
+            // key json file.
+            // string value = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+            // var credential = await GoogleCredential.GetApplicationDefaultAsync();
+
+            // Make sure you topicId doesn't contain '/' symbols.
+            var projectId = "{YOUR-PROJECT-ID}";
+            var topicId = "{YOUR-TOPIC-ID}";
+            var topicName = new TopicName(projectId, topicId);
+            var topic = await PublisherClient.CreateAsync(topicName);
+
+            var pubSubMessage = CreatePubSubMessage("Hello Ilya! This is my first message.");
+            await topic.PublishAsync(pubSubMessage);
+        }
+
+        private PubsubMessage CreatePubSubMessage(string message)
+        {
+            return new PubsubMessage
+            {
+                Data = ByteString.CopyFromUtf8(message),
+            };
+        }
+    }
+}
---
+++ SubscriberApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Google.Cloud.PubSub.V1;
+using System.Threading;
+
+namespace SubscriberApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().RunSubscriberAsync().Wait();
+
+            Console.WriteLine("\n\nTap to continue...");
+            Console.ReadKey();
+        }
+
+        private async Task RunSubscriberAsync()
+        {
+            // The GOOGLE_APPLICATION_CREDENTIALS environment variable contains the path to the
+            // key json file.
+            // string value = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS");
+            // var credential = await GoogleCredential.GetApplicationDefaultAsync();
+
+            // Make sure you subscriptionId doesn't contain '/' symbols.
+            var projectId = "{YOUR-PROJECT-ID}";
+            var subscriptionId = "{YOUR-SUBSCRIPTION-ID}";
+            var subscriptionName = new SubscriptionName(projectId, subscriptionId);
+
+            var subscription = await SubscriberClient.CreateAsync(subscriptionName);
+
+            await subscription.StartAsync(HandleMessage);
+        }
+
+        private Task<SubscriberClient.Reply> HandleMessage(PubsubMessage message, CancellationToken token)
+        {
+            var receivedMessage = message.Data.ToStringUtf8();
+            Console.WriteLine($"New message has been received!\nMessage: {receivedMessage}");
+
+            return Task.Run(() => SubscriberClient.Reply.Ack);
+        }
+    }
+}
